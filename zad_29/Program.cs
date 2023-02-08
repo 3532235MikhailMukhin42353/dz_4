93d9d804abd5167b4e90e12f83e6e47f62c08e2b
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2023-02-08 18:08:28 +0300

    sdfsdfsd :wq

---
+++ zad_29/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.*/
+
+Console.Write("Введите количество элементов в массиве: ");
+int elements = Convert.ToInt32(Console.ReadLine());
+int [] myArray = new int [elements];
+Zapolnenie(myArray);
+
+void Zapolnenie (int [] myArray)
+{
+    Console.WriteLine("Вывод массива: ");
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        myArray[i] = new Random().Next(-100,101);
+        Console.Write(myArray[i]+ "\t");
+    }
+}
